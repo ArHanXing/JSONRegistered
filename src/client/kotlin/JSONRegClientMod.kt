848f3f902448c99a575402b696577f2c8aa3d231
ArHanXing <mcvmware@Hotmail.com>
2026-05-30 22:32:29 +0800

    修复缓存失效

--- src/client/kotlin/JSONRegClientMod.kt
+++ src/client/kotlin/JSONRegClientMod.kt
@@ -4,19 +4,28 @@ import ar.hxi.jsonreg.JSONRegMod
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandler
 import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper
+import net.fabricmc.fabric.api.resource.ResourceReloadListenerKeys
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.texture.Sprite
-import net.minecraft.client.texture.SpriteAtlasTexture
+import net.minecraft.fluid.Fluid
 import net.minecraft.registry.Registries
+import net.minecraft.resource.ResourceManager
+import net.minecraft.resource.ResourceType
+import net.minecraft.screen.PlayerScreenHandler
 import net.minecraft.util.Identifier
-import net.minecraft.fluid.FluidState
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.BlockRenderView
 
 class JSONRegClientMod : ClientModInitializer {
+
+    private val spriteCache = mutableMapOf<Fluid, ResettableLazy<Array<Sprite?>>>()
+
     override fun onInitializeClient() {
         JSONRegMod.LOGGER.info("[JRegister] Client initialization started!")
 
-        // 注册流体渲染处理器
+        ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES)
+            .registerReloadListener(FluidSpriteReloadListener())
+
         registerFluidRendering()
 
         JSONRegMod.LOGGER.info("[JRegister] Client initialization complete!")
@@ -35,38 +44,64 @@ class JSONRegClientMod : ClientModInitializer {
             val textureStill = Identifier.of(JSONRegMod.MOD_ID, "block/${fluid.id}_still")
             val textureFlow = Identifier.of(JSONRegMod.MOD_ID, "block/${fluid.id}_flow")
 
-            val handler = object : FluidRenderHandler {
-                private var sprites: Array<Sprite?>? = null
-
-                override fun getFluidSprites(
-                    view: BlockRenderView?,
-                    pos: BlockPos?,
-                    state: FluidState?
-                ): Array<Sprite?> {
-                    if (sprites == null) {
-                        val atlas = SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE
-                        val textureManager = net.minecraft.client.MinecraftClient.getInstance().textureManager
-                        val atlasTexture = textureManager.getTexture(atlas) as? SpriteAtlasTexture
-
-                        if (atlasTexture != null) {
-                            sprites = arrayOf(
-                                atlasTexture.getSprite(textureStill),
-                                atlasTexture.getSprite(textureFlow)
-                            )
-                        } else {
-                            sprites = arrayOf(null, null)
-                        }
-                    }
-                    return sprites!!
-                }
+            val lazySprites = ResettableLazy {
+                val atlas = MinecraftClient.getInstance()
+                    .bakedModelManager
+                    .getAtlas(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE)
+                arrayOf(
+                    atlas.getSprite(textureStill),
+                    atlas.getSprite(textureFlow)
+                )
             }
+
+            val handler = FluidRenderHandler { _, _, _ -> lazySprites.get() }
 
             FluidRenderHandlerRegistry.INSTANCE.register(stillFluid, handler)
             FluidRenderHandlerRegistry.INSTANCE.register(flowingFluid, handler)
+
+            spriteCache[stillFluid] = lazySprites
+            spriteCache[flowingFluid] = lazySprites
 
             JSONRegMod.LOGGER.info("Registered fluid renderer for: ${fluid.id}")
         }
 
         JSONRegMod.LOGGER.info("Fluid rendering handlers registered successfully!")
+    }
+
+    private inner class FluidSpriteReloadListener : SimpleSynchronousResourceReloadListener {
+        override fun getFabricId(): Identifier =
+            Identifier.of(JSONRegMod.MOD_ID, "fluid_sprite_reloader")
+
+        override fun reload(manager: ResourceManager) {
+            spriteCache.values.forEach { it.reset() }
+        }
+
+        override fun getFabricDependencies(): Collection<Identifier> =
+            listOf(ResourceReloadListenerKeys.TEXTURES)
+    }
+
+    private class ResettableLazy<T>(private val supplier: () -> T) {
+        @Volatile
+        private var cached: T? = null
+
+        fun get(): T {
+            var value = cached
+            if (value == null) {
+                synchronized(this) {
+                    value = cached
+                    if (value == null) {
+                        value = supplier()
+                        cached = value
+                    }
+                }
+            }
+            return value!!
+        }
+
+        fun reset() {
+            synchronized(this) {
+                cached = null
+            }
+        }
     }
 }
